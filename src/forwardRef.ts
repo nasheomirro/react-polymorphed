73ddb77cc3deeac9fa212c079cb0103e766fa0d1
Kmyrrpen <shanenescarimoro@gmail.com>
2022-12-15 12:53:47 +0800

    found loophole for faster ComponentPropsWithRef<T>

--- src/forwardRef.ts
+++ src/forwardRef.ts
@@ -4,13 +4,14 @@ import React, {
   ForwardRefRenderFunction,
 } from "react";
 
-import { $Merge, PolyForwardExoticComponent, WithAs } from "./types";
+import { Merge, PolyForwardExoticComponent, WithAs } from "./types";
 
-/** 
+/**
  * React.forwardRef but typed in a way to support Polymorphic Components,
  * apart from typing it's just forwardRef with an extra call stack on top.
+ * Note that you should only use this for dealing with Polymorphic Components.
  */
-const forwardRef = <
+function forwardRef<
   Default extends ElementType = ElementType,
   P extends object = {},
   Loose extends "loose" | null = null
@@ -18,14 +19,11 @@ const forwardRef = <
   Component: ForwardRefRenderFunction<
     any,
     Loose extends "loose"
-      ? $Merge<ComponentPropsWithoutRef<Default>, WithAs<P>>
+      ? Merge<ComponentPropsWithoutRef<Default>, WithAs<P>>
       : WithAs<P>
   >
-) => {
-  return React.forwardRef(Component) as PolyForwardExoticComponent<
-    Default,
-    P
-  >;
-};
+) {
+  return React.forwardRef(Component) as PolyForwardExoticComponent<Default, P>;
+}
 
 export default forwardRef;
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 import {
+  Component,
   ComponentProps,
   ComponentPropsWithoutRef,
   ElementType,
@@ -6,11 +7,13 @@ import {
   LazyExoticComponent,
   MemoExoticComponent,
   NamedExoticComponent,
+  PropsWithoutRef,
   PropsWithRef,
   ReactElement,
+  RefAttributes,
 } from "react";
 
-export type $Merge<A, B> = Omit<A, keyof B> & B;
+export type Merge<A, B> = Omit<A, keyof B> & B;
 
 /** Adds `as` property as an optional prop if Props doesn't already have the `as` property. */
 export type WithAs<
@@ -18,22 +21,13 @@ export type WithAs<
   Type extends ElementType = ElementType
 > = { as?: Type } & Props;
 
-type PropsButWithRef<T extends ElementType> = PropsWithRef<
-  ComponentProps<T>
->;
-
 export type PolymorphicPropsWithoutRef<
   Component extends ElementType,
   Props extends object = {}
-> = $Merge<ComponentPropsWithoutRef<Component>, WithAs<Props, Component>>;
-
-export type PolymorphicPropsWithRef<
-  Component extends ElementType = ElementType,
-  Props extends object = {}
-> = $Merge<PropsButWithRef<Component>, WithAs<Props, Component>>;
+> = Merge<ComponentPropsWithoutRef<Component>, WithAs<Props, Component>>;
 
 /**
- * makes components polymorphic given the default prop type,
+ * makes components polymorphic given the default prop type
  */
 export type PolymorphicComponent<
   Default extends ElementType = ElementType,
@@ -44,30 +38,18 @@ export type PolymorphicComponent<
   ): ReactElement | null;
 };
 
-/**
- * adds the ref attribute to PolymorphicComponent, usually you shouldn't
- * have to use this when you can just use `forwardRef()`
- */
-export type PolymorphicComponentWithRef<
-  Default extends ElementType = ElementType,
-  Props extends object = {}
-> = {
-  <Component extends ElementType = Default>(
-    props: PolymorphicPropsWithRef<Component, Props>
-  ): ReactElement | null;
-};
-
-export type ManualPolymorphicProps<
+export type PolymorphicProps<
   DefaultProps extends object = {},
   ComponentProps extends object = {},
   Default extends ElementType = ElementType,
   Component extends ElementType = ElementType,
   Props extends object = {}
-> = $Merge<
+> = Merge<
   Component extends Default ? DefaultProps : ComponentProps,
   WithAs<Props, Component>
 >;
 
+// Don't ask me how this works either.
 /**
  * at definition, function arguments becomes the props of the default
  * component passed. **WARNING:** Though it is incorrect, as long as you only use
@@ -78,7 +60,7 @@ export type LoosePolymorphicComponent<
   Props extends object = {}
 > = {
   <Component extends ElementType = Default>(
-    props: ManualPolymorphicProps<
+    props: PolymorphicProps<
       ComponentPropsWithoutRef<Default>,
       ComponentPropsWithoutRef<Component>,
       Default,
@@ -88,18 +70,30 @@ export type LoosePolymorphicComponent<
   ): ReactElement | null;
 };
 
+// for some reason, removing PropsWithRef<T> from this type makes this a lot faster.
+// don't ask me how, it just does. The PropsWithRef<T> type was just lifted to the top.
+//
+// I'm guessing its because its not being cached by typescript and also its been
+// placed inside a conditional (`PropsWithRef<ComponentProps<T>>`), typescript is going
+// through it for every element type.
+export type _ComponentPropsWithRef<T extends ElementType> = T extends new (
+  props: infer P
+) => Component<any, any>
+  ? PropsWithoutRef<P> & RefAttributes<InstanceType<T>>
+  : ComponentProps<T>;
+
 /**
- * adds the ref attribute to `LoosePolymorphicComponent`, usually you shouldn't
- * have to use this when you can use `forwardRef()`.
+ * adds the ref attribute to PolymorphicComponent, usually you shouldn't
+ * have to use this as you can just use `forwardRef()`
  */
-export type LoosePolymorphicComponentWithRef<
+export type PolymorphicComponentWithRef<
   Default extends ElementType = ElementType,
   Props extends object = {}
 > = {
   <Component extends ElementType = Default>(
-    props: ManualPolymorphicProps<
-      PropsButWithRef<Default>,
-      PropsButWithRef<Component>,
+    props: PolymorphicProps<
+      PropsWithRef<_ComponentPropsWithRef<Default>>,
+      PropsWithRef<_ComponentPropsWithRef<Component>>,
       Default,
       Component,
       Props
@@ -118,7 +112,7 @@ export type LoosePolymorphicComponentWithRef<
 export type PolyForwardExoticComponent<
   Default extends ElementType,
   Props extends object = {}
-> = $Merge<
+> = Merge<
   ForwardRefExoticComponent<{}>,
   PolymorphicComponentWithRef<Default, Props>
 >;
@@ -126,12 +120,12 @@ export type PolyForwardExoticComponent<
 export type PolyNamedExoticComponent<
   Default extends ElementType,
   Props extends object = {}
-> = $Merge<NamedExoticComponent<{}>, PolymorphicComponent<Default, Props>>;
+> = Merge<NamedExoticComponent<{}>, PolymorphicComponent<Default, Props>>;
 
 export type PolyMemoExoticComponent<
   Default extends ElementType,
   Props extends object = {}
-> = $Merge<
+> = Merge<
   MemoExoticComponent<any>,
   PolymorphicComponentWithRef<Default, Props>
 >;
@@ -139,7 +133,7 @@ export type PolyMemoExoticComponent<
 export type PolyLazyExoticComponent<
   Default extends ElementType,
   Props extends object = {}
-> = $Merge<
+> = Merge<
   LazyExoticComponent<any>,
   PolymorphicComponentWithRef<Default, Props>
 >;
